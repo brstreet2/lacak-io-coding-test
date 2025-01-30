7b01d8fe601c76db45ba7a0b618bd23a603c911b
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2025-01-30 11:50:06 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.json({ limit: "11mb" }));
 app.use(express.urlencoded({ limit: "11mb", extended: true }));
-
+app.get("/", (req: Request, res: Response) => {
+  res.status(200);
+});
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({ message: "API is running." });
 });
